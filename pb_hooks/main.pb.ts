bb41ecce9df99e3df85e137af9c5abbad8363327
Bunnarin Chrun <chrunbunnarin857@gmail.com>
2026-01-27 12:23:58 +0700

    added welcme email and unsubscribe

--- pb_hooks/main.pb.ts
+++ pb_hooks/main.pb.ts
@@ -1,5 +1,18 @@
 /// <reference path="../pb_data/types.d.ts" />
 
+// welcome email to fake aws verifier
+onRecordCreate(e => {
+    $app.newMailClient().send(new MailerMessage({
+        from: {
+            address: $app.settings().meta.senderAddress,
+            name: $app.settings().meta.senderName,
+        },
+        to: [{ address: e.record?.get('email') }],
+        subject: `Welcome to Peacekhth.uk`,
+        html: `Welcome to Peacekhth.uk. click here to unsubscribe <a href="https://peacekhth.uk/unsubscribe/${e.record?.get('id')}">unsubscribe</a>`,
+    }));
+}, 'users')
+
 // remove the query params and encode the domain to save space
 onRecordCreate(e => {
     const url = e.record?.get('url').split('?')[0];
@@ -34,5 +47,12 @@ routerAdd('POST', '/new_member/{nationality}', e => {
     $app.db().newQuery(`
         UPDATE counter SET ${nationality} = ${nationality} + 1
     `).execute();
+    e.json(200);
+})
+
+routerAdd('POST', '/unsubscribe/{id}', e => {
+    $app.db().newQuery(`
+        DELETE FROM users WHERE id = {:id}
+    `).bind({ id: e.request?.pathValue("id") }).execute();
     e.json(200);
 })
